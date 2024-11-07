52dca3e31067870b3d4cdb1ce6e0ed82761ff39a
portfoliioo <allamhassan684@gmail.com>
2024-11-07 19:39:34 +0100

    Update script.js

--- Home/Projects/Websites/Maps/script.js
+++ Home/Projects/Websites/Maps/script.js
@@ -1,2 +1,2 @@
 document.getElementById("send").addEventListener("click", function(){
-    document.getElementById("map").setAttribute("src", "https://www.google.com/maps/embed/v1/place?q=" + document.getElementById("street").value +"+" + document.getElementById("city").value + "&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8")});
+    document.getElementById("map").setAttribute("src", "https://www.google.com/maps/embed/v1/place?q=" + document.getElementById("street").value +"+" + document.getElementById("city").value + "&key=d1a1ff541e9ff164dfa34d72693f3b22")});
