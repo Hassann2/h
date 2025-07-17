774e9ed71660b922d8714bc1d69d7e9868202a20
Hassan <allamhassan684@gmail.com>
2025-07-17 16:37:27 +0200

    Update script.js

--- Home/Projects/Websites/Maps/script.js
+++ Home/Projects/Websites/Maps/script.js
@@ -1,2 +1,6 @@
-document.getElementById("send").addEventListener("click", function(){
-    document.getElementById("map").setAttribute("src", "https://www.google.com/maps/embed/v1/place?q=" + document.getElementById("street").value +"+" + document.getElementById("city").value + "&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8")});
+document.getElementById("send").addEventListener("click", function() {
+    const key = "AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8";
+    const street = document.getElementById("street").value;
+    const city = document.getElementById("city").value;
+    document.getElementById("map").setAttribute("src", "https://www.google.com/maps/embed/v1/place?q=" + encodeURIComponent(street) + "+" + encodeURIComponent(city) + `&key=${key}`);
+});
