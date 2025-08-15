b403a5c38662af2de1c94412372833482f80d192
Hassan <allamhassan684@gmail.com>
2025-08-15 22:17:55 +0200

    Update remove_extension.js

--- remove_extension.js
+++ remove_extension.js
@@ -10,16 +10,32 @@
       history.replaceState(null, document.title, cleanUrl);
     }
   });
-})();
-
-(function () {
-    var origOpen = XMLHttpRequest.prototype.open;
-    XMLHttpRequest.prototype.open = function () {        
-        console.warn = function () { };
-        window['console']['warn'] = function () { };
-        this.addEventListener('load', function () {                        
-            console.warn('Something bad happened.');
-            window['console']['warn'] = function () { };
-        });        
-    };
+  // disable right click
+  document.oncontextmenu = function (e) {
+    e.preventDefault()
+    return false;
+}
+// disable somme additional key
+document.onkeydown = function (e) {
+    // disable f12
+    if (event.keyCode == 123) {
+        return false;
+    }
+    // disable ctrl+shift+I
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+    }
+    // disable ctrl+shift+C
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+        return false;
+    }
+    // disable ctrl+shift+j
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return false;
+    }
+    // disable ctrl+shift+U
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+    }
+}
 })();
