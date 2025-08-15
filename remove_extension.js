e7b62513c7d04ccae0149e112a0207a23f509ffb
Hassan <allamhassan684@gmail.com>
2025-08-15 18:38:58 +0200

    Update remove_extension.js

--- remove_extension.js
+++ remove_extension.js
@@ -1,42 +1,12 @@
 (function (){
-  function checkFileExists(url, callback){
-    const xhr = new XMLHttpRequest();
-    xhr.open('HEAD', url, true);
-    xhr.onreadystatechange = function () {
-      if(xhr.readyState === 4) {
-        callback(xhr.status >= 200 && xhr.status < 300);
-      }
-    };
-    xhr.send();
-  }
+  window.addEventListener('load', function (){
+    const url = this.window.location.pathname;
+    const extensions = /\.(html|php|aspx|htm|jsp|cfm|css|js) $/i;
 
-  const url = window.location.pathname;
-  const extensions = /\.(html|php|aspx|htm|jsp|cfm|css|js) $/i;
+    if(extensions.test(url)){
+      const cleanUrl = url.replace(extensions, '');
 
-  if(extensions.test(url)){
-    const cleanUrl = url.replace(extensions, '');
-    const possibleExtensions = ['.html', '.htm', '.php', '.aspx', '.jsp', '.cfm', '.css', '.js'];
-
-    function tryExtension(index){
-      if (index >= possibleExtensions.length){
-        return;
-      }
-
-      const testUrl = cleanUrl + possibleExtensions[index];
-      checkFileExists(testUrl,
-        function(exists){
-          if(exists){
-            window.location.replace(cleanUrl);
-          }else{
-            tryExtension(index + 1);
-          }
-        }
-      );
+      this.history.replaceState(null, this.document.title, cleanUrl);
     }
-
-    tryExtension(0);
-  }else if(url.endsWith('/') && url !== '/'){
-    const noSlashUrl = url.slice(0, -1);
-    window.location.replace(noSlashUrl);
-  }
+  });
 })();
