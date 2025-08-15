6fc7dcde3a03205cc0559c26d3cbbac66bd95c8a
Hassan <allamhassan684@gmail.com>
2025-08-15 18:57:52 +0200

    Update remove_extension.js

--- remove_extension.js
+++ remove_extension.js
@@ -1,11 +1,15 @@
-(function (){
-  window.addEventListener('load', function (){
-    const url = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname;
-    const extensions = /\.(html|php|aspx|htm|jsp|cfm|css|js) $/i;
-
-    if(extensions.test(url)){
-      const cleanUrl = url.replace(extensions, '');
-
+(function() {
+  window.addEventListener('load', function() {
+    // Ottieni l'URL corrente senza parametri query/hash
+    const path = window.location.pathname;
+    const extensions = /\.(html|php|aspx|htm|jsp|cfm|css|js)$/i; // Rimosso lo spazio
+    
+    if (extensions.test(path)) {
+      // Costruisci la nuova URL rimuovendo l'estensione
+      const cleanPath = path.replace(extensions, '');
+      const cleanUrl = window.location.origin + cleanPath;
+      
+      // Aggiorna l'URL senza ricaricare la pagina
       history.replaceState(null, document.title, cleanUrl);
     }
   });
